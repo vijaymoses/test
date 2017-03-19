5ef357501c72221229ebe0ded87acde9206e2ac6
vijay.moses@marks-and-spencer.com <vijay.moses@marks-and-spencer.com>
2017-03-19 14:14:43 +0530

    created batch file folders

---
+++ navigate.bat
@@ -0,0 +1,12 @@
+cd ../automation_1
+call calc.bat
+powershell -command "Start-Sleep -s 5"
+cd ../automation_2
+call calc.bat
+powershell -command "Start-Sleep -s 5"
+cd ../automation_3
+call calc.bat
+powershell -command "Start-Sleep -s 5"
+cd ../automation_4
+call calc.bat
+echo "success"
